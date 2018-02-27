7191fdfaee9930748aa05e43718df9785d9212ba
Juliano Rafael <juliano@getty.io>
2018-02-27 14:26:38 -0300

    fix webpack production config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -133,6 +133,10 @@ module.exports = {
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
         oneOf: [
+          {
+            test: /\.raw\.(js|css)$/,
+            loader: require.resolve('raw-loader')
+          },
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
